379fd8d698abc48f355ecc2fd4cb377311a51652
srilq <git@srilq.email>
2017-11-25 16:07:37 +0000

    fixes and tests

--- index.js
+++ index.js
@@ -9,24 +9,20 @@ function outerspace (strings, ...exprs) {
   if (!exprs.length) return strings.join('')
   if (!strings.length) return exprs.join('')
 
-  const firstExpr = exprs[0]
-  const lastExpr = exprs[exprs.length - 1]
+  const firstExpr = exprs[0] || ''
+  const lastExpr = exprs[exprs.length - 1] || ''
   const leadingSpace = firstExpr.match(/^\s*/)[0]
   const trailingSpace = lastExpr.match(/\s*$/)[0]
 
   let acc = ''
-  acc += leadingSpace
-  acc += strings[0]
-  acc += firstExpr.replace(/^\s*/, '')
-  if (strings.length > 1) {
-    acc = strings.slice(1, -1)
-        .reduce((acc, str, i) => acc + str + (exprs[i + 1] || ''), acc)
-  }
-  if (exprs.length > strings.length) {
-    acc += exprs.slice(strings.length - 1).join('')
-  }
-  acc = acc.replace(/\s*$/, '')
-  acc += strings[strings.length - 1]
-  acc += trailingSpace
+  acc += leadingSpace || ''
+  acc += strings[0] || ''
+  acc += firstExpr.replace(/^\s*/, '') || ''
+  acc = strings.slice(1, -1)
+      .reduce((acc, str, i) => acc + (str || '') + (exprs[i + 1] || ''), acc)
+  acc += exprs.slice(strings.length - 1).join('') || ''
+  if (acc.trim()) acc = acc.replace(/\s*$/, '')
+  acc += strings[strings.length - 1] || ''
+  acc += trailingSpace || ''
   return acc
 }
--- test.js
+++ test.js
@@ -14,3 +14,33 @@ test('explode multiple', function (t) {
   t.equal(outerspace`**${'\n\n Holy'} ${'Exclamation '}${'Batman! \n'}**`, '\n\n **Holy Exclamation Batman!** \n')
   t.end()
 })
+
+test('explode empty string', function (t) {
+  t.plan(1)
+  t.equal(outerspace``, '')
+  t.end()
+})
+
+test('explode only whitespace', function (t) {
+  t.plan(1)
+  t.equal(outerspace`  `, '  ')
+  t.end()
+})
+
+test('explode empty expr', function (t) {
+  t.plan(1)
+  t.equal(outerspace` ${''} `, '  ')
+  t.end()
+})
+
+test('explode null expr', function (t) {
+  t.plan(1)
+  t.equal(outerspace` ${null} `, '  ')
+  t.end()
+})
+
+test('explode undefined expr', function (t) {
+  t.plan(1)
+  t.equal(outerspace` ${void 0} `, '  ')
+  t.end()
+})

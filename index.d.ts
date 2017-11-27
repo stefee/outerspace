ef7bf94ba54e68a6c33e72f12a2e274886570247
srilq <git@srilq.email>
2017-11-27 18:24:26 +0000

    fix type definition

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 export = outerspace;
 declare function outerspace(strings: TemplateStringsArray, ...exprs: any[]): string;
 declare namespace outerspace {
+  export function wrap(before: string, s: string, after: string): string;
   export function before(s: string, before: string): string;
   export function after(s: string, after: string): string;
 }
